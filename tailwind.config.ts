6c316fb84363ce7819c1da307ff54cc6f5b9b4ce
Jaka Daneu <jakad@me.com>
2023-11-09 17:12:36 +0100

    feat: 💄 adds brand and accent color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,6 @@
+// https://www.color-name.com/hex/09afda
+// https://www.tints.dev/brand/09AFDA
+import { fontFamily } from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -7,9 +10,39 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    extend: {
+      colors: {
+        accent: {
+          50: '#E2F8FE',
+          100: '#C9F2FD',
+          200: '#93E6FB',
+          300: '#5DD9F8',
+          400: '#27CDF6',
+          500: '#09AFDA',
+          600: '#078FB0',
+          700: '#056B84',
+          800: '#044758',
+          900: '#02242C',
+          950: '#011014',
+        },
+        brand: {
+          50: '#E7F9FD',
+          100: '#D5F4FB',
+          200: '#AAE8F8',
+          300: '#7BDCF4',
+          400: '#51D1F1',
+          500: '#26C5ED',
+          600: '#11A5CB',
+          700: '#0C7B97',
+          800: '#095568',
+          900: '#042A34',
+          950: '#021318',
+        },
+      },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
       },
+    },
   },
   plugins: [],
 };
