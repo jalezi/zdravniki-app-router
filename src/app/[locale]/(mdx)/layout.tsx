2d6948c0ec90a50f722b9f605487e7a74ccd87ac
Jaka Daneu <jakad@me.com>
2023-11-17 19:15:02 +0100

    feat: adds Footer component to MDX layout

--- src/app/[locale]/(mdx)/layout.tsx
+++ src/app/[locale]/(mdx)/layout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react';
 
+import { Footer } from '@/components/footer';
+
 export default function MdxLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <main className='relative md:mt-6 mx-auto max-w-3xl px-4 pb-8 md:px-0 bg-white shadow-md'>
-        <div className='mx-auto py-4 prose prose-sm prose-a:transition-all prose-a:duration-367 prose-li:marker:text-inherit'>
+      <main className='relative mx-auto max-w-3xl bg-white px-4 pb-8 shadow-md md:mt-6 md:px-0'>
+        <div className='prose prose-sm mx-auto py-4 prose-a:transition-all prose-a:duration-367 prose-li:marker:text-inherit'>
           {children}
         </div>
       </main>
-      <footer>Footer</footer>
+      <Footer variant='mdx' />
     </>
   );
 }
