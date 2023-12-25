fb1d93f1ad570585e8811904cf2796a4c0e894fa
Jaka Daneu <jakad@me.com>
2023-12-25 11:02:10 +0100

    fix: ✏️ wrong toc english translation

--- src/locales/en/mdx.ts
+++ src/locales/en/mdx.ts
@@ -1,7 +1,7 @@
 export const mdx = {
   emergencyInfo:
     'In the event that you require emergency medical assistance, please contact your local emergency service or call 112.',
-  toc: 'Table of Contents',
+  toc: 'Table of Content',
   sidebar: 'Sidebar',
   progressBar: 'Progress Bar',
   mobile: 'Mobile',
