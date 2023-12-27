3fb9b62f4d30caeecd40cbbc6e7b5d71c1c31975
Jaka Daneu <jakad@me.com>
2023-12-27 18:39:22 +0100

    feat: ✨ implements slugify functionality
    Use slugify package.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,38 @@
+import slugify from 'slugify';
+
+import { Locales } from '@/locales/config';
+
 export { getSiteUrl } from './getSiteUrl';
+
+type SlugOptions = {
+  replacement?: string;
+  remove?: RegExp;
+  lower?: boolean;
+  strict?: boolean;
+  locale?: string;
+  trim?: boolean;
+};
+
+export const toSlug = function toSlug(
+  text = '',
+  options: SlugOptions = { lower: true, remove: /[*+~.()'"!:@]/g, trim: true }
+) {
+  return slugify(text, options);
+};
+
+export const createToSlugLocale = (locale?: Locales, options?: SlugOptions) => {
+  const _options = options ?? {
+    lower: true,
+    remove: /[*+~.()'"!:@]/g,
+    locale: locale ?? undefined,
+    trim: true,
+  };
+
+  return (text: string) => toSlug(text, _options);
+};
+
+export const toSlugLocale = {
+  sl: createToSlugLocale('sl'),
+  en: createToSlugLocale('en'),
+  it: createToSlugLocale('it'),
+};
