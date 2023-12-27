648b88d8554566727334930463dc2394a95b15e9
Jaka Daneu <jakad@me.com>
2023-12-27 15:17:36 +0100

    feat: ✨ adds function to fetch doctors & institutions CSV files

--- src/lib/get-csv.ts
+++ src/lib/get-csv.ts
@@ -1,3 +1,4 @@
+import { DOCTORS_CSV_URL, INSTITUTIONS_CSV_URL } from './constants/url';
 import HTTPError from './errors/HTTPError';
 import ValidationError from './errors/ValidationError';
 import { urlCsvSchema } from './schemas';
@@ -25,4 +26,30 @@ export const getCsv = async (url: string | URL, revalidate: number = 3600) => {
     message: `Invalid URL\n${safeUrl.error.message}`,
     context: { url, ...safeUrl.error },
   });
+};
+
+export const getDoctorsAndInstitutinsCsv = async (
+  revalidate: number = 3600
+) => {
+  const promises = [
+    getCsv(DOCTORS_CSV_URL, revalidate),
+    getCsv(INSTITUTIONS_CSV_URL, revalidate),
+  ];
+
+  const [doctors, institutions] = await Promise.all(promises);
+
+  if (doctors?.data && institutions?.data) {
+    return {
+      data: { doctors: doctors.data, institutions: institutions.data },
+      error: null,
+    };
+  }
+
+  return {
+    data: null,
+    error: new ValidationError({
+      message: 'Something went wrong during fetching CSVs',
+      context: { doctors, institutions },
+    }),
+  };
 };
