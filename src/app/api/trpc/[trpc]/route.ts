8429d847ff1e01d9f7593b78e102a17d988aa5dc
Jaka Daneu <jakad@me.com>
2023-12-29 20:29:03 +0100

    fix: 🧑‍💻 makes trpc routes cache control different in development

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,19 +2,20 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
 import { appRouter } from '@/trpc/server';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const handler = (req: Request) =>
   fetchRequestHandler({
     req,
     router: appRouter,
     createContext: () => ({}),
-    onError:
-      process.env.NODE_ENV === 'development'
-        ? ({ path, error }) => {
-            console.error(
-              `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
-            );
-          }
-        : undefined,
+    onError: isDevelopment
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
+          );
+        }
+      : undefined,
     endpoint: '/api/trpc',
     responseMeta({ type, errors }) {
       const allOk = errors.length === 0;
@@ -24,10 +25,15 @@ const handler = (req: Request) =>
       if (isQuery && allOk) {
         // cache request for 1 day + revalidate once every second
         const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+        const ONE_MINUTE_IN_SECONDS = 60;
         return {
-          headers: {
-            'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-          },
+          headers: isDevelopment
+            ? {
+                'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_MINUTE_IN_SECONDS}`,
+              }
+            : {
+                'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+              },
         };
       }
       return {};
