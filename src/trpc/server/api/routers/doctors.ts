f51c43c4d51449586edf8483c2168f4b53e5957e
Jaka Daneu <jakad@me.com>
2023-12-31 10:50:25 +0100

    refactor: ♻️ better error handling in trpc.fdoctors.getPage

--- src/trpc/server/api/routers/doctors.ts
+++ src/trpc/server/api/routers/doctors.ts
@@ -3,7 +3,6 @@ import { doctorsQueryInputSchema } from '@/lib/schemas';
 import {
   fetchAndParseDoctorsAndInstitutions,
   getSiteUrl,
-  getStartAndEnd,
   filters,
 } from '@/lib/utils';
 
@@ -16,87 +15,61 @@ export const doctorsRouter = router({
       doctorsQueryInputSchema.default({ type: 'all', page: 1, pageSize: 25 })
     )
     .query(async ({ input }) => {
-      const { data, errors } = await fetchAndParseDoctorsAndInstitutions();
-      if (errors) {
-        return { data: null, error: errors, success: false } as const;
-      }
-
-      if (data?.doctors && data?.institutions) {
-        const { doctors, institutions } = data;
-        const { type, page, pageSize } = input;
-
-        const { paginatedDoctors, uniqueInstitutions, total } =
-          filters.getPaginatedDoctorsWithUniqueInstitutions(
-            doctors,
-            institutions,
-            input
-          );
-
-        const { end } = getStartAndEnd(page, pageSize);
-        const prevPage = page > 1 ? page - 1 : null;
-        const nextPage = end < total ? page + 1 : null;
-
-        const createPageUrl = (_page: number | null) => {
-          if (_page === null) {
-            return null;
-          }
-
-          const url = new URL('api/trpc/doctors.getPage', getSiteUrl());
-          url.searchParams.append(
-            'input',
-            JSON.stringify({ json: { type, page: _page, pageSize } })
-          );
-
-          return url.pathname + url.search;
-        };
-
-        const prev = createPageUrl(prevPage);
-        const next = createPageUrl(nextPage);
-
-        return {
-          data: {
-            doctors: paginatedDoctors,
-            institutions: uniqueInstitutions,
-            page,
-            pageSize,
-            prevPage: prev,
-            nextPage: next,
-            total,
-          },
-          error: null,
-          success: true,
-        } as const;
-      }
-
-      if (!data?.doctors) {
+      const { data, errors, success } =
+        await fetchAndParseDoctorsAndInstitutions();
+      if (!data || !success || !data.doctors || !data.institutions || !errors) {
         return {
           data: null,
           error: new ValidationError({
-            message: 'No doctors data!',
-            context: { data, errors },
+            message: 'Could not fetch/parse doctors or institutions',
+            context: { errors, data },
           }),
           success: false,
         } as const;
       }
 
-      if (!data?.institutions) {
-        return {
-          data: null,
-          error: new ValidationError({
-            message: 'No institutions data!',
-            context: { data, errors },
-          }),
-          success: false,
-        } as const;
-      }
+      const { doctors, institutions } = data;
+      const { type, page, pageSize } = input;
+
+      const { paginatedDoctors, uniqueInstitutions, total, end } =
+        filters.getPaginatedDoctorsWithUniqueInstitutions(
+          doctors,
+          institutions,
+          input
+        );
+
+      const prevPage = page > 1 ? page - 1 : null;
+      const nextPage = end < total ? page + 1 : null;
+
+      const createPageUrl = (_page: number | null) => {
+        if (_page === null) {
+          return null;
+        }
+
+        const url = new URL('api/trpc/doctors.getPage', getSiteUrl());
+        url.searchParams.append(
+          'input',
+          JSON.stringify({ json: { type, page: _page, pageSize } })
+        );
+
+        return url.pathname + url.search;
+      };
+
+      const prev = createPageUrl(prevPage);
+      const next = createPageUrl(nextPage);
 
       return {
-        data: null,
-        error: new ValidationError({
-          message: 'Should not happen!',
-          context: { data, errors },
-        }),
-        success: false,
+        data: {
+          doctors: paginatedDoctors,
+          institutions: uniqueInstitutions,
+          page,
+          pageSize,
+          prevPage: prev,
+          nextPage: next,
+          total,
+        },
+        error: null,
+        success: true,
       } as const;
     }),
 });
