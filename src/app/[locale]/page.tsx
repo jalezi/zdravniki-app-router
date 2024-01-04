39b48a1bdc3901fc1a8b6c45c7347fcb7c777b5e
Jaka Daneu <jakad@me.com>
2024-01-04 16:24:34 +0100

    refactor: 🗑️ cleanup for new attemp

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,17 +3,12 @@ import { Metadata } from 'next';
 import { setStaticParamsLocale } from 'next-international/server';
 
 import { DoctorTypeListSection as Section } from '@/components/cards';
-import { Dialog } from '@/components/dialog';
 import MdxFooter from '@/components/footer/MdxFooter';
-import {
-  fetchAndParseDoctorsAndInstitutions,
-  filters,
-  toSlug,
-} from '@/lib/utils';
+import { fetchAndParseDoctorsAndInstitutions, toSlug } from '@/lib/utils';
+import { getInstitutionsMap, groupDoctorsByType } from '@/lib/utils/filters';
 import { getI18n, getScopedI18n, getStaticParams } from '@/locales/server';
 
-import { Form } from './form';
-import { defaultsSearchParamsSchema, redirectWithSearchParams } from './utils';
+import { defaultsSearchParamsSchema } from './utils';
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getScopedI18n('seo');
@@ -55,51 +50,21 @@ export default async function Home({
     ? parsedParams.data
     : defaultsSearchParamsSchema.parse({});
 
-  const { paginatedDoctors, uniqueInstitutions, total, end, start } =
-    filters.getPaginatedDoctorsWithUniqueInstitutions(
-      doctors,
-      institutions,
-      parsedSearchParams
-    );
-
-  const areSearchparamsInvalid =
-    !parsedParams.success ||
-    Object.keys(searchParams).some(
-      key => !parsedParams.data[key as keyof typeof parsedSearchParams]
-    );
-
-  async function onClose() {
-    'use server';
-    if (!areSearchparamsInvalid) {
-      return;
-    }
-
-    const defaultParams = defaultsSearchParamsSchema.parse({});
-    redirectWithSearchParams(defaultParams, locale);
-  }
+  const doctorGroupsByType = groupDoctorsByType(doctors);
+  const doctorsByType = doctorGroupsByType.get(parsedSearchParams.type) ?? [];
+  const uniqueInstitutions = getInstitutionsMap(doctorsByType, institutions);
 
   return (
     <>
       <main id='content' className='mx-auto mt-12 max-w-7xl px-4 py-4 md:mt-16'>
         <h1 className='sr-only'>{t('test')}</h1>
-        <Form total={total} />
-        <Dialog
-          showModal={areSearchparamsInvalid ? true : undefined}
-          close={onClose}
-          closeTimeout={3000}
-        >
-          Invalid params
-        </Dialog>
+
         <div className='flex flex-col gap-4'>
           <Section>
             <div>
               <h2>{parsedSearchParams.type.toLocaleUpperCase()}</h2>
-              <p>
-                Showing from {start + 1} to {end > total ? total : end} of{' '}
-                {total} doctors
-              </p>
               <ul>
-                {paginatedDoctors.map(doctor => {
+                {doctorsByType.map(doctor => {
                   const doctorSlugify = toSlug(doctor.doctor);
                   const doctorHref = `/${doctor.type}/${doctorSlugify}/${doctor.id_inst}/`;
                   const inst = uniqueInstitutions.get(doctor.id_inst);
