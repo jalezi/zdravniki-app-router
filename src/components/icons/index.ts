84f070e2a26950e070864ac50f671e20737c1ae6
Jaka Daneu <jakad@me.com>
2024-01-08 16:48:52 +0100

    feat: 🍱 adds extra and floating clinic icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -9,6 +9,10 @@ export { default as YoutubeIcon } from '@/assets/svg/icon-youtube.svg';
 export { default as Covid19Icon } from '@/assets/svg/icon-covid19.svg';
 export { default as PodnebnikIcon } from '@/assets/svg/icon-podnebnik.svg';
 export { default as StudentsIcon } from '@/assets/svg/icon-students.svg';
-export {default as GynecologistIcon} from '@/assets/svg/icon-gynecologist.svg';
+export { default as GynecologistIcon } from '@/assets/svg/icon-gynecologist.svg';
 export { default as KidsIcon } from '@/assets/svg/icon-kids.svg';
 export { default as DentistIcon } from '@/assets/svg/icon-dentist.svg';
+export { default as ExtraClinicIcon } from '@/assets/svg/icon-clinic.svg';
+export { default as FloatingClinicIcon } from '@/assets/svg/icon-gp-floating.svg';
+
+export { DOCTOR_ICONS } from './doctor-icons';
