73fe0abcdc7d7ad8596958cb93a0696da59e270a
Jaka Daneu <jakad@me.com>
2024-01-09 19:01:06 +0100

    fix: 🐛 storybook does not recognize imports
    tunseccussedful attempt to fix storybook rendering RSC

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,27 @@
+import path from 'path';
+
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {
+      nextConfigPath: path.resolve(__dirname, '../next.config.mjs'),
+    },
   },
-  "docs": {
-    "autodocs": "tag"
-  }
+  docs: {
+    autodocs: 'tag',
+  },
+  features: {
+    // @ts-expect-error
+    experimentalNextRSC: true,
+  },
 };
 export default config;
