b1506cfd18f5afe0799b13d1873df2d402e4d226
Jaka Daneu <jakad@me.com>
2024-01-14 11:06:35 +0100

    fix: 🔍️ broken skip links on pages different than faq & about

--- src/components/skip-links/SkipLinks.tsx
+++ src/components/skip-links/SkipLinks.tsx
@@ -1,13 +1,29 @@
-import { getScopedI18n } from '@/locales/server';
+'use client';
+import { usePathname } from 'next/navigation';
+
+import { useCurrentLocale, useScopedI18n } from '@/locales/client';
+
+import { ROUTES_TRANSLATIONS } from 'rewrites-redirects.config.mjs';
 
 import SkipLink from './SkipLink';
 
-export default async function SkipLinks() {
-  const t = await getScopedI18n('skipLinks');
+export default function SkipLinks() {
+  const t = useScopedI18n('skipLinks');
+  const locale = useCurrentLocale();
+  const pathnameTranslations = ROUTES_TRANSLATIONS[locale];
+  const canonicalPathname = usePathname();
+  const hasToc =
+    canonicalPathname &&
+    [pathnameTranslations.about, pathnameTranslations.faq].some(path =>
+      canonicalPathname.includes(path)
+    );
+
   return (
     <ul id='nav-access' className='fixed -top-80 z-[10000] w-full'>
       <li>
-        <SkipLink href='#nav-main-toggler'>{t('menuToggler')}</SkipLink>
+        <SkipLink className='md:hidden' href='#nav-main-toggler'>
+          {t('menuToggler')}
+        </SkipLink>
       </li>
       <li className='hidden md:list-item'>
         <SkipLink href='#lang-selector'>{t('langSelector')}</SkipLink>
@@ -15,12 +31,16 @@ export default async function SkipLinks() {
       <li>
         <SkipLink href='#content'>{t('main')}</SkipLink>
       </li>
-      <li className='md:hidden'>
-        <SkipLink href='#sidebar-toggle'>{t('tocToggler')}</SkipLink>
-      </li>
-      <li className='hidden md:list-item'>
-        <SkipLink href='#desktop-toc-nav'>{t('toc')}</SkipLink>
-      </li>
+      {hasToc ? (
+        <>
+          <li className='md:hidden'>
+            <SkipLink href='#sidebar-toggle'>{t('tocToggler')}</SkipLink>
+          </li>
+          <li className='hidden md:list-item'>
+            <SkipLink href='#desktop-toc-nav'>{t('toc')}</SkipLink>
+          </li>
+        </>
+      ) : null}
     </ul>
   );
 }
