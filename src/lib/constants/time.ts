9d1a3292b82ab34578f3b08b4d7f333237a7ddd4
Jaka Daneu <jakad@me.com>
2024-01-16 18:18:31 +0100

    refactor: 🧑‍💻 not using calculations for time constants

--- src/lib/constants/time.ts
+++ src/lib/constants/time.ts
@@ -1,6 +1,6 @@
-export const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+export const ONE_DAY_IN_SECONDS = 86_400; // 24 * 60 * 60
 
-export const ONE_HOUR_IN_SECONDS = 60 * 60;
+export const ONE_HOUR_IN_SECONDS = 3600; // 60 * 60
 export const ONE_MINUTE_IN_SECONDS = 60;
 
 export const ONE_SECOND_IN_MILLISECONDS = 1000;
