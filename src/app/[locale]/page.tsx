b7dab1c6a609d6798c2b8bd67677981a04a80314
Jaka Daneu <jakad@me.com>
2024-02-06 16:45:23 +0100

    fix: 💄 doctor card to narrow on lg breakpoint

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -105,7 +105,7 @@ export default async function Home({
             </div>
           )}
 
-          <ul className='grid grid-cols-1 gap-x-3 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 '>
+          <ul className='grid grid-cols-1 gap-x-3 gap-y-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 '>
             <Suspense
               key={parsedSearchParams.toString()}
               fallback={
