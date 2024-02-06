deb1838d27bbc592fb1c77b7b16a866c2558331d
Jaka Daneu <jakad@me.com>
2024-02-06 20:00:19 +0100

    fix: 🐛 name not properly highlighted
    when there is space in query

--- src/components/cards/doctor/Name.tsx
+++ src/components/cards/doctor/Name.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Fragment, ReactNode } from 'react';
+import { Fragment } from 'react';
 
 import Link, { LinkProps } from 'next/link';
 import { useSearchParams } from 'next/navigation';
@@ -29,40 +29,29 @@ export default function Name({
 
   const isMatch = query && fullMatch(name, query);
 
-  const highlightedName: ReactNode[] = [];
+  const highlightedParts: [boolean, number, number][] = [];
 
   if (isMatch) {
-    const splittedName = name.split(' ');
-    const normalizedQuery = normalize(query);
+    const normalizedName = normalize(name);
+    const normalizedQuery = normalize(query)
+      .split(' ')
+      .sort((a, b) => b.length - a.length);
 
-    splittedName.forEach((word, i, arr) => {
-      const index = normalize(word).indexOf(normalizedQuery);
-      const key = word + i + arr.length + query;
-
-      if (index === -1) {
-        highlightedName.push(
-          <Fragment key={key}>
-            {word}
-            {i < arr.length ? ' ' : ''}
-          </Fragment>
-        );
+    normalizedQuery.forEach(q => {
+      const index = normalizedName.indexOf(q);
+      if (
+        index === -1 ||
+        highlightedParts.some(p => p[1] === index) ||
+        index === index + q.length
+      ) {
         return;
       }
-
-      const before = word.slice(0, index);
-      const match = word.slice(index, index + query.length);
-      const after = word.slice(index + query.length);
-
-      highlightedName.push(
-        <Fragment key={key}>
-          {before}
-          {match && <mark>{match}</mark>}
-          {after}
-          {i < arr.length ? ' ' : ''}
-        </Fragment>
-      );
+      highlightedParts.push([true, index, index + q.length]);
     });
   }
+
+  const sortedParts = [...highlightedParts.sort((a, b) => a[1] - b[1])];
+  const filledParts = fillGaps(sortedParts, name.length);
 
   const styles = cn(
     'text-xl font-bold focus-within:text-brand-500 hover:text-brand-500 transition-all duration-367',
@@ -71,10 +60,55 @@ export default function Name({
 
   return (
     <Link href={href} className={styles}>
-      <Heading as={as} {...props}>
-        {isMatch ? highlightedName : name}
+      <Heading
+        as={as}
+        {...props}
+        title={name}
+        className='child:transition-all child:duration-367 child:hover:bg-inherit child:hover:text-brand-500'
+      >
+        {isMatch
+          ? filledParts.map((item, i) => {
+              const [isHighlighted, start, end] = item;
+              return (
+                <Fragment key={i}>
+                  {isHighlighted ? (
+                    <mark className=''>{name.substring(start, end)}</mark>
+                  ) : (
+                    name.substring(start, end)
+                  )}
+                </Fragment>
+              );
+            })
+          : name}
+
         <span className='sr-only'>{nameSrOnly}</span>
       </Heading>
     </Link>
   );
+}
+
+// fill in the gaps
+// before [true, 0, 0], [true, 8, 10], [true, 15, 20]
+// after [false 0, 8], [true, 8, 10], [false, 10, 15], [true,15, 20]
+
+// before [true, 13, 17]
+// after [false, 0, 13], [true, 13, 17]
+
+// also pass the length of the string to the function and use it to fill the gaps
+
+function fillGaps(parts: [boolean, number, number][], length: number) {
+  const result: [boolean, number, number][] = [];
+  let start = 0;
+  for (const part of parts) {
+    const [isHighlighted, partStart, partEnd] = part;
+    if (start < partStart) {
+      result.push([false, start, partStart]);
+    }
+    result.push([isHighlighted, partStart, partEnd]);
+    start = partEnd;
+  }
+  if (start < length) {
+    result.push([false, start, length]);
+  }
+  return result;
 }
