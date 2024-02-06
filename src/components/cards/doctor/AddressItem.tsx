121a4bc60ddf9a3a905897c62b3d1cd5a1ffd715
Jaka Daneu <jakad@me.com>
2024-02-06 16:40:14 +0100

    feat: ✨ adds highlighted text for matching query

--- src/components/cards/doctor/AddressItem.tsx
+++ src/components/cards/doctor/AddressItem.tsx
@@ -1,8 +1,13 @@
-import { HTMLAttributes } from 'react';
+'use client';
+
+import { Fragment, HTMLAttributes, ReactNode } from 'react';
+
+import { useSearchParams } from 'next/navigation';
 
 import { VariantProps, cva } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import { normalize, partialMatch } from '@/lib/utils/search';
 
 const addressItemVariants = cva('', {
   variants: {
@@ -37,6 +42,47 @@ const AddressItem = function AddressItem({
   className,
   ...props
 }: AddressItemProps) {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query');
+
+  const isMatch =
+    typeof text === 'string' && query ? partialMatch([text], query) : false;
+
+  const highlightedText: ReactNode[] = [];
+
+  if (isMatch && typeof text === 'string' && query) {
+    const splittedText = text.split(' ');
+    const normalizedQuery = normalize(query);
+
+    splittedText.forEach((word, i, arr) => {
+      const index = normalize(word).indexOf(normalizedQuery);
+      const key = word + i + arr.length + query;
+
+      if (index === -1) {
+        highlightedText.push(
+          <Fragment key={key}>
+            {word}
+            {i < arr.length ? ' ' : ''}
+          </Fragment>
+        );
+        return;
+      }
+
+      const before = word.slice(0, index);
+      const match = word.slice(index, index + query.length);
+      const after = word.slice(index + query.length);
+
+      highlightedText.push(
+        <Fragment key={key}>
+          {before}
+          {match && <mark>{match}</mark>}
+          {after}
+          {i < arr.length ? ' ' : ''}
+        </Fragment>
+      );
+    });
+  }
+
   return (
     <span
       className={cn(
@@ -46,7 +92,7 @@ const AddressItem = function AddressItem({
       title={title}
       {...props}
     >
-      {children ?? text ?? title}
+      {children ?? isMatch ? highlightedText : text ?? title}
     </span>
   );
 };
--- src/components/cards/doctor/Name.tsx
+++ src/components/cards/doctor/Name.tsx
@@ -1,8 +1,14 @@
+'use client';
+
+import { Fragment, ReactNode } from 'react';
+
 import Link, { LinkProps } from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 import { Heading } from '@/components/ui/headings';
 import type { HeadingProps } from '@/components/ui/headings';
 import { cn } from '@/lib/utils';
+import { fullMatch, normalize } from '@/lib/utils/search';
 
 export interface NameProps extends Omit<HeadingProps, 'children'> {
   href: LinkProps['href'];
@@ -10,7 +16,7 @@ export interface NameProps extends Omit<HeadingProps, 'children'> {
   nameSrOnly?: string;
 }
 
-export default async function Name({
+export default function Name({
   as,
   className,
   href,
@@ -18,6 +24,46 @@ export default async function Name({
   nameSrOnly,
   ...props
 }: NameProps) {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query');
+
+  const isMatch = query && fullMatch(name, query);
+
+  const highlightedName: ReactNode[] = [];
+
+  if (isMatch) {
+    const splittedName = name.split(' ');
+    const normalizedQuery = normalize(query);
+
+    splittedName.forEach((word, i, arr) => {
+      const index = normalize(word).indexOf(normalizedQuery);
+      const key = word + i + arr.length + query;
+
+      if (index === -1) {
+        highlightedName.push(
+          <Fragment key={key}>
+            {word}
+            {i < arr.length ? ' ' : ''}
+          </Fragment>
+        );
+        return;
+      }
+
+      const before = word.slice(0, index);
+      const match = word.slice(index, index + query.length);
+      const after = word.slice(index + query.length);
+
+      highlightedName.push(
+        <Fragment key={key}>
+          {before}
+          {match && <mark>{match}</mark>}
+          {after}
+          {i < arr.length ? ' ' : ''}
+        </Fragment>
+      );
+    });
+  }
+
   const styles = cn(
     'text-xl font-bold focus-within:text-brand-500 hover:text-brand-500 transition-all duration-367',
     className
@@ -26,7 +72,7 @@ export default async function Name({
   return (
     <Link href={href} className={styles}>
       <Heading as={as} {...props}>
-        {name}
+        {isMatch ? highlightedName : name}
         <span className='sr-only'>{nameSrOnly}</span>
       </Heading>
     </Link>
